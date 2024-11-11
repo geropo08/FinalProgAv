57c70192bf179f907c3eac2e8e1508a81a875cea
geropo08 <84402339+geropo08@users.noreply.github.com>
2024-11-11 15:19:31 -0300

    Prueba

--- Entregable1/main.py
+++ Entregable1/main.py
@@ -2,5 +2,5 @@ from funciones import *
 import leer_csv
 if __name__ == "__main__":
 
-    datos = leer_csv.leer_csv('reduced_question_set.csv')
+    datos = leer_csv.leer_csv('reduced_question_test.csv')
     iniciar(datos)
