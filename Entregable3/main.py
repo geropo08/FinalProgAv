13e034993fca6ee0e8d72f7b288e43bc5b49247c
geropo08 <84402339+geropo08@users.noreply.github.com>
2024-11-11 21:57:56 -0300

    CambiosRap

--- Entregable3/main.py
+++ Entregable3/main.py
@@ -8,68 +8,69 @@ if __name__ == "__main__":\
 
     print("1- Traductor de SQL")
     print("2- Fluent API")
-    trabajo=input('Ingrese opcion: ')
-    if trabajo=="1" :
+    print("Ingrese una opcion")
+    #trabajo=input('Ingrese opcion: ')
+    #if trabajo=="1" :
 
-        idioma = input('Ingrese el idioma al cual traducir: ')
-        if idioma.lower()=='español':
-            dar_vuelta()
-        while True:
-            try:
-                if idioma.lower()=="español":
-                    s = input('Consulta SQL en ingles: ')
-                else:
-                    s = input('Consulta SQL en español: ')
-            except EOFError:
-                break
-            if not s:
-                print("Error:query vacia")
-                continue
-            result = parser.parse(s)
-            print("Consulta traducida:", result)
-            try:
-                if idioma.lower()=='español':
-                    resp=cursor.execute(s)
-                else:
-                    resp=cursor.execute(result)
-                
-                for fila in resp:
-                        print(fila)
-            except Exception as e:
-                if "no such table" in str(e):
-                    print("Alguna tabla no existe")
-                elif "no such colum" in str(e):
-                    print("Algun atributo no existe")
-                else:
-                    print("Consulta no valida")
-    elif trabajo=="2":
-        "Se debe crear"
+    #    idioma = input('Ingrese el idioma al cual traducir: ')
+    #    if idioma.lower()=='español':
+    #        dar_vuelta()
+    #   while True:
+    #        try:
+    #            if idioma.lower()=="español":
+    #                s = input('Consulta SQL en ingles: ')
+    #            else:
+    #                s = input('Consulta SQL en español: ')
+    #        except EOFError:
+    #            break
+    #        if not s:
+    #            print("Error:query vacia")
+    #            continue
+    #        result = parser.parse(s)
+    #        print("Consulta traducida:", result)
+    #        try:
+    #            if idioma.lower()=='español':
+    #                resp=cursor.execute(s)
+    #            else:
+    #                resp=cursor.execute(result)
+    #            
+    #            for fila in resp:
+    #                    print(fila)
+    #        except Exception as e:
+    #            if "no such table" in str(e):
+    #                print("Alguna tabla no existe")
+    #            elif "no such colum" in str(e):
+    #                print("Algun atributo no existe")
+    #            else:
+    #                print("Consulta no valida")
+    #elif trabajo=="2":
+    #    "Se debe crear"
         
-        consulta1 = Consulta(idioma="en")
-        query1 = (consulta1
-                .select()
-                .everything()
-                .from_table()
-                .nombre("usuarios")
-                .where()
-                .nombre("edad")
-                .gr_op()
-                .ls_params("18")
-                .get_query())
-        print(query1)
-        try:
-            resp=cursor.execute(query1)
-                
-            for fila in resp:
-                print(fila)
-        except Exception as e:
-            if "no such table" in str(e):
-                print("Alguna tabla no existe")
-            elif "no such colum" in str(e):
-                print("Algun atributo no existe")
-            else:
-                print("Consulta no valida")
+    #    consulta1 = Consulta(idioma="en")
+    #    query1 = (consulta1
+    #            .select()
+    #            .everything()
+    #            .from_table()
+    #            .nombre("usuarios")
+    #            .where()
+    #            .nombre("edad")
+    #            .gr_op()
+    #            .ls_params("18")
+    #            .get_query())
+    #    print(query1)
+    #    try:
+    #        resp=cursor.execute(query1)
+    #            
+    #        for fila in resp:
+    #            print(fila)
+    #    except Exception as e:
+    #        if "no such table" in str(e):
+    #            print("Alguna tabla no existe")
+    #        elif "no such colum" in str(e):
+    #            print("Algun atributo no existe")
+    #        else:
+    #            print("Consulta no valida")
 
 
-    else:
-        print("Opcion invalida")
+    #else:
+    #    print("Opcion invalida")
